ce18e3c5d4936bce536dd24d4cd62e38b6bd097f
TooFatToPogo <44614683+TooFatToPogo@users.noreply.github.com>
2020-02-26 13:08:18 +0100

    Initial scaffold of component structure

---
+++ src/components/HeadNav.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header d-flex justify-content-between">
+    <div class="d-flex">
+      <div id="logo" class="p-3">
+        <h1 class="text-center">
+          Super <br />
+          Cykelisterne
+        </h1>
+      </div>
+      <nav class="navbar navbar-expand-lg">
+        <ul class="navbar-nav">
+          <li class="nav-item m-2">
+            <router-link class="nav-link text-dark font-weight-bold" to="/">
+              Forside
+            </router-link>
+          </li>
+          <li class="nav-item m-2">
+            <router-link class="nav-link text-dark font-weight-bold" to="/medlemmer">
+              Medlemmer
+            </router-link>
+          </li>
+          <li class="nav-item m-2">
+            <router-link class="nav-link text-dark font-weight-bold" to="/om-os">
+              Om klubben
+            </router-link>
+          </li>
+          <li class="nav-item m-2">
+            <router-link class="nav-link text-dark font-weight-bold" to="/konto">
+              Medlemsområde
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeadNav"
+};
+</script>
+
+<style scoped>
+.nav-link:hover {
+  text-decoration: underline;
+}
+#logo {
+  background-color: #fcd12a;
+}
+.header {
+  border-bottom: 10px solid #fcd12a;
+}
+</style>
