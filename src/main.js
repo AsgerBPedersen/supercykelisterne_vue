5f60fe626c718c41f34785c64c8abd8e3480b75c
TooFatToPogo <44614683+TooFatToPogo@users.noreply.github.com>
2020-02-26 14:51:36 +0100

    setup apollo cient

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import App from "./App.vue";
 import VueRouter from "vue-router";
 import { BootstrapVue } from "bootstrap-vue";
 import { routes } from "./router";
+import ApolloClient from "./apolloCient";    
+
+
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import VueApollo from "vue-apollo";
 
+Vue.use(VueApollo);
 Vue.use(BootstrapVue);
 Vue.use(VueRouter);
 
@@ -17,7 +22,13 @@ const router = new VueRouter({
 
 Vue.config.productionTip = false;
 
+
+const apolloProvider = new VueApollo({
+  defaultClient: ApolloClient
+})
+
 new Vue({
   render: h => h(App),
-  router
+  router,
+  provide: apolloProvider.provide()
 }).$mount("#app");
