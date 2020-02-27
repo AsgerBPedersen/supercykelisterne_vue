95e3ca6eb46a503d96b65f974d0ea9cc77dff63e
TooFatToPogo <44614683+TooFatToPogo@users.noreply.github.com>
2020-02-27 11:50:22 +0100

    added credentials to apollo client

--- src/apolloCient.js
+++ src/apolloCient.js
@@ -2,6 +2,7 @@ import ApolloClient from 'apollo-boost';
 
 const apolloClient = new ApolloClient({
   // You should use an absolute URL here
+  credentials: 'include',
   uri: 'http://localhost:4000'
 });
 
