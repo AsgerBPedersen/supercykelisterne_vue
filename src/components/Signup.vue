cc2cc1cfcc58c7577e2de4d4ba94e9af304c2edc
TooFatToPogo <44614683+TooFatToPogo@users.noreply.github.com>
2020-02-27 11:50:57 +0100

    signup component

---
+++ src/components/Signup.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="col-sm-4">
+    <form method="post" v-on:submit="createUser($event)">
+      <h4 class="text-center p-3">OPRET BRUGER</h4>
+      <div class="form-group row d-flex justify-content-end">
+        <label class="form-label m-0 text-right p-2">Din email</label>
+        <div class="col-sm-8 p-0">
+          <input
+            class="form-control"
+            type="email"
+            placeholder="email"
+            name="email"
+            v-model="email"
+          />
+        </div>
+      </div>
+      <div class="form-group row d-flex justify-content-end">
+        <label class="form-label m-0 text-right p-2">Dit navn</label>
+        <div class="col-sm-8 p-0">
+          <input
+            class="form-control"
+            type="text"
+            placeholder="navn"
+            name="name"
+            v-model="name"
+          />
+        </div>
+      </div>
+      <div class="form-group row d-flex justify-content-end">
+        <label class="form-label m-0 text-right p-2">Kodeord</label>
+        <div class="col-sm-8 p-0">
+          <input
+            class="form-control"
+            type="password"
+            placeholder="kodeord"
+            name="password"
+            v-model="password"
+          />
+        </div>
+      </div>
+      <div class="form-group row d-flex justify-content-end">
+        <label class="form-label m-0 text-right p-2">Gentag</label>
+        <div class="col-sm-8 p-0">
+          <input
+            class="form-control"
+            type="password"
+            name="confirm"
+            placeholder="kodeord"
+            v-model="confirmPassword"
+          />
+        </div>
+      </div>
+      <div class="form-group d-flex justify-content-end">
+        <button class="btn btn-primary" type="submit">
+          LOG IND
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import {gql} from "apollo-boost";
+
+export default {
+  name: "signup",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: ""
+    };
+  },
+  methods: {
+    createUser(e) {
+      e.preventDefault();
+      this.$apollo.mutate({
+        mutation: gql`
+          mutation SIGNUP($email: String!, $name: String!, $password: String!) {
+            createUser(email: $email, name: $name, password: $password) {
+              id
+              email
+              name
+            }
+          }
+        `,
+        variables: {
+          email: this.email,
+          name: this.name,
+          password: this.password
+        }
+      }).then((data) => {
+      // Result
+      console.log(data)
+    }).catch((error) => {
+      // Error
+      console.error(error)
+      // We restore the initial user input
+    });
+      
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,16 +1,18 @@
 <template>
   <div>welcome
 
-  <carousel></carousel>
+  <!-- <carousel></carousel> -->
+  <signup></signup>
   </div>
 </template>
 
 <script>
-import Carousel from "./Carousel";
-
+// import Carousel from "./Carousel";
+import Signup from "./Signup";
 export default {
     components: {
-        Carousel
+        // Carousel
+        Signup
     }
 }
 </script>
