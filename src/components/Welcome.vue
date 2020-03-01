f1aa25652acfcfc5d0e8bc4075188ab1d9e13fa7
unknown <inzolin2@gmail.com>
2020-03-01 21:48:04 +0100

    add signin component

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -2,17 +2,30 @@
   <div>welcome
 
   <!-- <carousel></carousel> -->
-  <signup></signup>
+  <div class="d-flex justify-content-around container p-5">
+  <signup v-if="!state.user"></signup>
+  <signin v-if="!state.user"></signin>        
+        </div>
   </div>
 </template>
 
 <script>
 // import Carousel from "./Carousel";
 import Signup from "./Signup";
+import Signin from "./Signin";
+import { store } from "../store";
+
 export default {
+  name: "welcome",
+  data() {
+    return {
+      state: store.state
+    }
+  },
     components: {
         // Carousel
-        Signup
+        Signup,
+        Signin
     }
 }
 </script>
