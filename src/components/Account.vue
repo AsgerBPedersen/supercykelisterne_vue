2b370756f940b54e28240b08dc62491114ca8cb9
unknown <inzolin2@gmail.com>
2020-03-01 21:45:48 +0100

    add account component

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>account</div>
+  <div>
+    <div v-if="!state.user">
+      <h2 class="text-center m-5">
+        Log venligst ind eller opret en bruger
+      </h2>
+      <div class="d-flex justify-content-around container">
+        <signin></signin>
+        <signup></signup>
+      </div>
+    </div>
+    <EditAccount v-if="state.user"></EditAccount>
+  </div>
 </template>
 
 <script>
-export default {
+import Signup from "./Signup";
+import Signin from "./Signin";
+import EditAccount from "./EditAccount";
+import { store } from "../store";
 
-}
+export default {
+  name: "account",
+  data() {
+    return {
+      state: store.state
+    };
+  },
+  components: {
+    Signup,
+    Signin,
+    EditAccount
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
